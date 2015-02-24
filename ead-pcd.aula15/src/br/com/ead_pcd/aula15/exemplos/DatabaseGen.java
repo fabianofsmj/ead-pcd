dd6487c4f7bbce8be5a95bd23cd0331a5f25ea66
fabianofsmj <fabiano.fsmj@gmail.com>
2015-02-23 21:36:40 -0300

    Aula 15 - Criação das páginas

---
+++ ead-pcd.aula15/src/br/com/ead_pcd/aula15/exemplos/DatabaseGen.java
@@ -0,0 +1,19 @@
+package br.com.ead_pcd.aula15.exemplos;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DatabaseGen {
+	public static void main(String[] args) {
+		Properties config = new Properties();
+		config.put("hibernate.hbm2ddl.auto", "create");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("Aula15PU", config);
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		em.getTransaction().commit();
+	}
+
+}
